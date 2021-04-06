31e92edf1d41ab330d151ab3f8a201c05610eddd
lillapulay <59831508+lillapulay@users.noreply.github.com>
2021-04-06 09:06:05 +0200

    getAllCountries function done

--- assignment-2.js
+++ assignment-2.js
@@ -1,1 +1,24 @@
-// Your code here
+// Creating a class
+class Countries {
+    constructor(apiURL) {
+        this.apiURL = apiURL;
+    }
+
+    // Return a list of all countries
+    getAllCountries() {
+        fetch(`${this.apiURL}/all`)
+            .then(
+                res => res.json()
+                )
+            .then(
+                countries => {
+                    console.log('List of ALL countries:', countries)
+                })
+            .catch (err => console.log(err.message))
+    }
+}
+
+const myCountries = new Countries ('https://restcountries.eu/rest/v2')
+
+// Calling functions
+// myCountries.getAllCountries()
