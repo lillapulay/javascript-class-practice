ca73661418b44d9e953e0ed6ded9b340ef34f052
lillapulay <59831508+lillapulay@users.noreply.github.com>
2021-04-07 08:43:40 +0200

    GetCountryByName and GetBorderingCountries functions done

--- assignment-2.js
+++ assignment-2.js
@@ -4,17 +4,44 @@ class Countries {
         this.apiURL = apiURL;
     }
 
-    // Return a list of all countries
+    // Returns a list of all countries
     getAllCountries() {
         fetch(`${this.apiURL}/all`)
-            .then(
+            .then (
                 res => res.json()
                 )
-            .then(
+            .then (
                 countries => {
-                    console.log('List of ALL countries:', countries)
+                    countries.map(country => {
+                        console.log(country)
+                    })
+                    //console.log(countries)
                 })
             .catch (err => console.log(err.message))
+    }
+
+    // Returns a single country by name/native name
+    getCountryByName(name) {
+        fetch(`${this.apiURL}/name/${name}`)
+        .then (
+            res => res.json()
+        )
+        .then (
+            country => console.log(country)
+        )
+        .catch (err => console.log(err.message))
+    }
+
+    // Returns bordering countries
+    getBorderingCountries(name) {
+        fetch(`${this.apiURL}/name/${name}`)
+        .then (
+            res => res.json()
+        )
+        .then (
+            country => console.log(country[0].borders)
+        )
+        .catch (err => console.log(err.message))
     }
 }
 
@@ -22,3 +49,6 @@ const myCountries = new Countries ('https://restcountries.eu/rest/v2')
 
 // Calling functions
 // myCountries.getAllCountries()
+// myCountries.getCountryByName("Hungary")
+// myCountries.getCountryByName("Nederland") // Doesn't work with "Magyarország" for example - must be something with the accent on 'a' but couldn't figure
+// myCountries.getBorderingCountries("Finland")
